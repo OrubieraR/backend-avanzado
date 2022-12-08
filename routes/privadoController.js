ab0d5e4582b2061d5f3b83c00ba65c417970b700
OrubieraR <107748119+OrubieraR@users.noreply.github.com>
2022-12-08 20:15:02 +0100

    Añadido el email del usuario que se ha logado en la página.

--- routes/privadoController.js
+++ routes/privadoController.js
@@ -1,8 +1,21 @@
 "use strict";
+const { Usuario } = require("../models");
 
 class PrivadoController {
-  index(req, res, next) {
-    res.render("privado");
+  async index(req, res, next) {
+    try {
+      const usuarioId = req.session.usuarioLogado;
+      const usuario = await Usuario.findById(usuarioId);
+
+      if (!usuario) {
+        next(new Error("Usuario no encontrado"));
+        return;
+      }
+
+      res.render("privado", { email: usuario.email });
+    } catch (err) {
+      next(err);
+    }
   }
 }
 
